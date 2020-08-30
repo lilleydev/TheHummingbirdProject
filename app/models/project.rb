8723d401b0456aafe12a6bf9e53c2e36d78d7703
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-08-29 19:31:09 -0700

    update associations for project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
-  has_many :users, through: :user_project
+  has_many :users, through: :user_projects
   has_many :user_projects
   has_many :reviews
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
