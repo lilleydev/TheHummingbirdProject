3c4b192768286eeb4b354130eb88fb7fd0c88e33
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-08-30 21:34:51 -0700

    create session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,4 +7,14 @@ class SessionsController < ApplicationController
   end
 
   def new; end
+
+  def create
+    user = User.find_by(username: params[:user][:username])
+    if user&.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to "/users/#{user.id}"
+    else
+      redirect_to '/login'
+    end
+  end
 end
