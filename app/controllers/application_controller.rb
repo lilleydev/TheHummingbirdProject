6835c4855b3604edfff7c726f6b298b8eeafe40c
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-08-30 21:47:43 -0700

    add current_user methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def redirect_if_logged_out
+    redirect_to '/' unless current_user
+  end
 end
