25c4af0c56aa624a406b927fbec89243a4bc4d00
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-02 20:53:47 -0700

    edit layout links and add controller actions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,3 +1,15 @@
 class ProjectsController < ApplicationController
   before_action :redirect_if_logged_out
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    binding.pry
+    @project = Project.new(project_params)
+    if @project.save
+
+    end
+  end
 end
