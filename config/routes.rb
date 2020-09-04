5a571853f729f98a4cf4288c0b9ddb1bbce8eca1
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-03 20:19:27 -0700

    add route devise_for

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'registrations/Devise::RegistrationsController'
-  # devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
+
   root to: 'sessions#home'
 
   # #add custom routes here
