f2668c86162bcca1dcf7d5be8c37c5c4cea0ece2
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-05 11:56:33 -0700

    delete unneeded model methods Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,2 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
-
-  def create
-    user = User.create(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to user
-    else
-      render :new
-    end
-  end
-
-  def show
-    # redirect_if_logged_out
-    # @user = User.find_by_id(params[:id])
-    # redirect_to '/' unless @user
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:username, :first_name, :last_name, :password, :email)
-  end
 end
